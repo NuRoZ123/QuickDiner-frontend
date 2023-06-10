bac621f2c2413244d33bf0c250f14b05e205c3bb
Georgio Sorin <georgio.sorin@gmail.com>
2023-06-10 15:05:07 +0200

    correctif pas

--- src/components/filter/kmComponent.vue
+++ src/components/filter/kmComponent.vue
@@ -2,7 +2,7 @@
     <div class="flex flex-col w-auto justify-center">
       <label for="prixMoyen" class="font-bold my-1">distance des restaurants</label>
       <div class="flex flex-row items-center justify-center">
-        <input v-model="storeRestaurants.filter.km" class="w-40 h-1 mr-4 bg-gray-200 rounded-lg appearance-none cursor-pointer range-sm" type="range" id="prixMoyen" name="prixMoyen" min="0" :max="_.round(storeRestaurants.km)" step="10">
+        <input v-model="storeRestaurants.filter.km" class="w-40 h-1 mr-4 bg-gray-200 rounded-lg appearance-none cursor-pointer range-sm" type="range" id="km" name="km" min="0" :max="_.round(storeRestaurants.km)" step="10">
         <span class="">{{storeRestaurants.filter.km}} km</span>
       </div>
 
--- src/components/filter/prixMoyenComponent.vue
+++ src/components/filter/prixMoyenComponent.vue
@@ -2,7 +2,7 @@
     <div class="flex flex-col w-auto justify-center">
       <label for="prixMoyen" class="font-bold my-1">Tarif moyen des restaurants</label>
       <div class="flex flex-row items-center justify-center">
-        <input v-model="storeRestaurants.filter.avgPrice" class="w-40 h-1 mr-4 bg-gray-200 rounded-lg appearance-none cursor-pointer range-sm" type="range" id="prixMoyen" name="prixMoyen" min="0" :max="_.round(storeRestaurants.maxPrice)" step="5">
+        <input v-model="storeRestaurants.filter.avgPrice" class="w-40 h-1 mr-4 bg-gray-200 rounded-lg appearance-none cursor-pointer range-sm" type="range" id="prixMoyen" name="prixMoyen" min="0" :max="_.round(storeRestaurants.maxPrice)" step="1">
         <span class="">{{storeRestaurants.filter.avgPrice}} €</span>
       </div>
 
